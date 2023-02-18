7d105e8cef1ea2a8cc352816e33e3cc2987dcb4a
fito <fito.prolog@gmail.com>
2023-02-18 14:13:17 -0800

    adapted captioning image model

---
+++ software/brain/create_input_files.py
@@ -0,0 +1,11 @@
+from utils import create_input_files
+
+if __name__ == '__main__':
+    # Create input files (along with word map)
+    create_input_files(dataset='flickr8k',
+                       karpathy_json_path='/home/fito/Downloads/datasets/dataset_flickr8k.json',
+                       image_folder='/home/fito/Downloads/datasets/images/',
+                       captions_per_image=5,
+                       min_word_freq=5,
+                       output_folder='./ledata/',
+                       max_len=50)
