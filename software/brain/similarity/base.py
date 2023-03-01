9fb749f345cd913e7888067151509747f332e32b
fito <fito.prolog@gmail.com>
2023-02-28 20:10:27 -0800

    added joint

---
+++ software/brain/similarity/base.py
@@ -0,0 +1,13 @@
+from sentence_transformers import SentenceTransformer, util
+
+model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+
+sentence1 = "RFX13SSAS"
+
+while True:
+  sentence2= input(">")
+  if sentence2=="quit": break
+  embeddings = model.encode([sentence1, sentence2])
+  cosine_similarity = util.cos_sim(embeddings[0], embeddings[1])
+  print(cosine_similarity)
+
